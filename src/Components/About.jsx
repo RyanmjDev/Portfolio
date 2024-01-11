5966121a1e227021a32d6b3885f9e7ba953a0de1
RyanmjDev <Ryanmj.dev@gmail.com>
2024-01-11 14:36:26 -0600

    Update About section with personal story

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const About = () => {
   return (
-    <section id="about" className="py-12 bg-fixed bg-center bg-cover flex justify-center items-center"  >
+    <section id="about" className=" h-screen bg-fixed bg-center bg-cover flex justify-center items-center"  >
       <div className="container mx-auto px-4 w-full xl:w-1/2" data-aos="fade-up" data-aos-once="false" data-aos-duration="1000">
         <div className=" mx-auto text-center"> 
         <h2 className="text-4xl font-bold mb-8">
@@ -10,7 +10,8 @@ const About = () => {
           </h2>
           <div className="py-4 rounded-lg ">
             <p className="text-lg leading-relaxed">
-            I have cultivated a strong passion for creating elegant, responsive websites using modern technologies as a self taught developer. 
+            From a young age, I was deeply fascinated by both playing and developing video games. This early interest led me to learn C++ at the age of 14,
+            a stepping stone that ignited my journey into coding. Since then I have cultivated a strong passion for creating elegant, responsive websites using modern technologies as a self taught developer. 
             I pride myself on staying current with the latest trends in web development, ensuring that my projects consistently exceed expectations.
             When I'm not coding, I love to explore new challenges and expanding my skillset to continue delivering exceptional results. 
             I'm always looking to take my skills to the next level!
